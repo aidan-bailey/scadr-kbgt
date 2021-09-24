27d45e937b5c134802e42f7c1252b2d39bc0f9c3
aidan <BLYAID001@myuct.ac.za>
2021-09-24 23:39:32 +0200

    Added GenerateRanked algorithm

--- src/main/scala/generation/KBGenerator.scala
+++ src/main/scala/generation/KBGenerator.scala
@@ -127,4 +127,46 @@ object KBGenerator {
     ddg.ChainDCGKB(defeasibleOnly)
   }
 
+  def GenerateRanked(
+      r: Int,
+      s: Int,
+      d: (Int, Int, Int) => Int,
+      defeasibleOnly: Boolean
+  ): MixedKnowledgeBase = {
+    val k = new MixedKnowledgeBase()
+    var lowerBound = 1
+    if (defeasibleOnly)
+      lowerBound = lowerBound + 1
+    val contradictionAtom = Atom("B")
+    var j = 0
+    while (j < r) {
+      val anchorAtom = Atom(s"$j")
+      if (j == 0) {
+        k += DefeasibleFormula(anchorAtom, contradictionAtom)
+        if (defeasibleOnly) {
+          var paddingAtom = Atom("d")
+          k += DefeasibleFormula(paddingAtom, anchorAtom)
+        }
+      } else {
+        val prevAnchor = Atom((j - 1).toString())
+        if (defeasibleOnly)
+          k += DefeasibleFormula(anchorAtom, prevAnchor)
+        else
+          k += BinCon(BinOp.Implies, anchorAtom, prevAnchor)
+        if ((j + 1) % 2 == 0)
+          k += DefeasibleFormula(anchorAtom, UnCon(UnOp.Not, contradictionAtom))
+        else
+          k += DefeasibleFormula(anchorAtom, contradictionAtom)
+      }
+      var i = 0
+      while (i < d(s, r, j + 1) - lowerBound) {
+        val extensionAtom = Atom(s"$j.$i")
+        k += DefeasibleFormula(extensionAtom, anchorAtom)
+        i += 1
+      }
+      j += 1
+    }
+    return k
+  }
+
 }
